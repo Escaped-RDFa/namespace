b62c6ee99db91cd7c659b3fcf30da17c4a19c21b
mike <h@solfunmeme.com>
2025-09-07 21:21:39 +0000

    feat: Migrate documentation to mdBook

---
+++ book_src/SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+- [Introduction](./README.md)
+- [Index](./index.md)
+- [Version 1.0](./1.0.md)
+- [Version 1.0 RSS](./1.0.rss.md)
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "An executable specification for the Escaped-RDFa Namespace.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mdbook-pkg = pkgs.mdbook;
+      in
+      {
+        packages = {
+          # The static site documentation
+          default = pkgs.stdenv.mkDerivation {
+            name = "namespace-spec-site";
+            src = ./.;
+            nativeBuildInputs = [ mdbook-pkg ];
+            buildPhase = ''
+              mdbook build
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r book/* $out/
+            '';
+          };
+
+          # The machine-readable "smart contract"
+          smart-contract = pkgs.runCommand "smart-contract.json" {} ''
+            echo '{ "version": "1.0", "spec": "..." }' > $out
+          '';
+        };
+      }
+    );
+}
